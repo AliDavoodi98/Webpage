cdebbe19063887c8f421a9d250896c4311cbd96a
Ali Davoodi Moghadam <ada131@sfu.ca>
2022-04-20 13:13:38 -0700

    Configure18

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Route exact path="/" component={Main} />
       <Route exact path="/about" component={AboutPage} />
       <Route exact path="/projects" component={MyProjects} />
-      <Route exact path="/work" component={WorkPage} />
+      <Route exact path="/work2" component={WorkPage} />
       <Route exact path="/skills" component={MySkillsPage} />
     </Switch>
 
